6ecc954159befa6cc1921e99a5681b8d7588ad72
Roozbeh Sayadi <roozbehsayadi@gmail.com>
2019-08-24 22:09:46 +0430

    Third Day

---
+++ MinMaxAndSortingDictionaries.py
@@ -0,0 +1,9 @@
+stocks = {
+	'GOOG': 520.54,
+	'FB': 76.45,
+	'YHOO': 39.28,
+	'AMZN': 306.21,
+	'AAPL': 99.76
+}
+
+print(sorted(zip(stocks.values(), stocks.keys())))
---
+++ UnpackListOrTuples.py
@@ -0,0 +1,10 @@
+date, name, price = ['December 23, 2015', 'Bread Gloves', 8.51]
+print(name)
+
+def drop_first_last(grades):
+	first, *middle, last = grades
+	avg = sum(middle) / len(middle)
+	print(avg)
+
+
+drop_first_last([65, 76, 98, 54, 21])
